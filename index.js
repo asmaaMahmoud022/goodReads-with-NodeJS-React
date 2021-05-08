7975ef38c667b8f69b8d737ca1ea402887776d81
asmaaMahmoud022 <asmaa_mahmoud022@yahoo.com>
2021-05-08 17:02:43 +0200

    add .gitignore file

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const app= express()
+const port = 3000
+
+mongoose.connect('mongodb://localhost:27017/goodreads',(err) => {
+    if(err)
+    {
+        console.error(err)
+        return
+    }else{
+        console.log("connected to db successfully")
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
