9ef2c52c1b3ac0530f63a5c020d3475bca5212c7
Mohamedelbeshlawy <mohamedelbeshlawyy@gmail.com>
2021-05-10 23:59:48 +0200

    Editing in Book Model

--- models/book.js
+++ models/book.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const bookSchema = new mongoose.Schema({
     name: { type: String, required: [true, "can't be blank"], unique: [true, 'Name must be unique'] },
-    image: { type: String, required: [true, "can't be blank"] },
+    cover: { type: String, required: [true, "can't be blank"] },
     author: { type: mongoose.Schema.Types.ObjectId, ref: 'Author' },
     category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' }
 })
