3e8b3c22bdfa4a6a92673f86c61baa719c65014f
Ahmed Ibrahim <ahmedibrahem22322@gmail.com>
2021-05-21 09:52:25 +0200

    adding the flexible nav and footer

---
+++ frontend/src/components/user/footer/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+var style = {
+  backgroundColor: "#F8F8F8",
+  borderTop: "1px solid #E7E7E7",
+  textAlign: "center",
+  padding: "20px",
+  position: "fixed",
+  left: "0",
+  bottom: "0",
+  height: "60px",
+  width: "100%",
+};
+
+var phantom = {
+  display: "block",
+  padding: "20px",
+  height: "60px",
+  width: "100%",
+};
+
+function Footer() {
+  return (
+    <div>
+      <div style={phantom} />
+      <div style={style}>
+        <a href="https://github.com/aAbdelnaby88/Good-Reads/" target="_blank">
+          By ITI students - cloud platfrom development track - intake 40
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
