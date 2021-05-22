dcb34ce16472a58b92981069cc962bb0158f8c9e
nagyadel1992 <nagy.adel10@yahoo.com>
2021-05-22 20:30:56 +0200

    Editing index

--- index.js
+++ index.js
@@ -1,34 +1,51 @@
-const express = require("express")
-const mongoose = require("mongoose")
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const cors = require("cors");
 const authRouter = require("./routes/auth");
-const categoryRouter = require("./routes/category")
-const bookRouter = require("./routes/book")
-const authorRouter = require("./routes/author")
+const authMWare = require("./middlewares/authMWare");
+const app = express();
+const authorRouter = require("./routes/authors");
+const bookRouter = require("./routes/books");
+const categRouter = require("./routes/category");
+const searchRouter = require("./routes/search");
 const rateRouter = require("./routes/rate");
 const reviewRouter = require("./routes/reviews");
-const app = express()
-const port = 3000
+const todoRouter = require("./routes/todo");
 
-app.use(express.json())
-app.use('/categories',categoryRouter)
-app.use('/books',bookRouter)
-app.use('/authors',authorRouter)
-app.use('/rates',rateRouter)
-app.use("/reviews", reviewRouter);
-app.use('/users',authRouter)
-mongoose.connect('mongodb://localhost:27017/goodreads', {
-    useUnifiedTopology: true,
+app.use(express.json());
+app.use(cors());
+app.use(bodyparser.json());
+app.use(express.static("./public"));
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/books", bookRouter);
+app.use("/api/authors", authorRouter);
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use("/api/", authRouter);
+app.use("/api/categories", categRouter);
+app.use("/api/search", searchRouter);
+app.use("/api/rates", rateRouter);
+app.use("/api/reviews", reviewRouter);
+app.use("/api/todos", todoRouter);
+
+app.get("/*", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+mongoose.connect(
+  "mongodb://localhost:27017/good-reads",
+  {
+    useCreateIndex: true,
     useNewUrlParser: true,
-    }, (err) => {
-            if(err)
-            {
-                console.error(err)
-                return
-            } else{
-                console.log("connected to db successfully")
-            }
-    })
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  },
+  (err) => {
+    if (!err) console.log("Mongod Connected...");
+  }
+);
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(5000, (err) => {
+  if (!err) return console.log("welcome to good reads server");
+  console.log(err);
+});
