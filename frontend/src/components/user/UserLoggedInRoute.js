1c63a03bde81c82c6ddf0c74ad3d9b905a661b61
Ahmed Ibrahim <ahmedibrahem22322@gmail.com>
2021-05-23 20:27:18 +0200

    configuring the route of the logged in users after fixing the home bug

---
+++ frontend/src/components/user/UserLoggedInRoute.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { getUserToken } from "../../utils";
+
+export default ({ component: Component, ...rest }) => {
+  const token = getUserToken();
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        token ? <Redirect to="/dashboard" /> : <Component {...props} />
+      }
+    />
+  );
+};
