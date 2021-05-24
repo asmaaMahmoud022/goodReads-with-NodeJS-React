45dee076925a57d35a21cd6660568b703e6a90fd
IslamReda <asoeslam@gmail.com>
2021-05-24 08:36:49 +0200

    add media files

---
+++ public/precache-manifest.1a2ad3f46b1e615b2636dde793786a4e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f4ad7cb3b8d2b9defbc99d89e89a4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cfc3ee779b6c3d8e6a5",
+    "url": "/static/css/2.7e1b122b.chunk.css"
+  },
+  {
+    "revision": "2be4ee0ade9f2e49925d",
+    "url": "/static/css/main.3962b7bc.chunk.css"
+  },
+  {
+    "revision": "2cfc3ee779b6c3d8e6a5",
+    "url": "/static/js/2.eddcd65c.chunk.js"
+  },
+  {
+    "revision": "8b0b0209d23e8c647d2b956557587522",
+    "url": "/static/js/2.eddcd65c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2be4ee0ade9f2e49925d",
+    "url": "/static/js/main.7e4ddf53.chunk.js"
+  },
+  {
+    "revision": "c71849e6b92a54d9b65d",
+    "url": "/static/js/runtime-main.dbac8e30.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
