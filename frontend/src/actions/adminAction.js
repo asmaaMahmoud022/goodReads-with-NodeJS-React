e326ac2213a1413b678980686c7e82beefe08d59
IslamReda <asoeslam@gmail.com>
2021-05-24 08:30:44 +0200

    fix bugs admin actions

--- frontend/src/actions/adminAction.js
+++ frontend/src/actions/adminAction.js
@@ -28,3 +28,21 @@ export const mergeAdminProps = (payload) => (dispatch) => {
   });
 };
 
+export const loginAdmin = (email, password) => (dispatch) => {
+  console.log(email, password);
+  axios
+    .post(`${API_HOST}/admin/login`, { username: email, password })
+    .then((data) => {
+      const { token } = data.data;
+      dispatch(updateAdminProps([{ prop: "token", value: token }]));
+      showMessage(
+        "Login Success!",
+        "Welecome, redirecting to admin panel...",
+        "success"
+      );
+    })
+    .catch((err) => {
+      handleError(err);
+    });
+};
+
